ebcd592a64c52ed420312f5d2f28eac6f44fd7e6
unknown <dima>
2025-12-17 15:04:57 +0300

    some

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,13 +1,23 @@
 import './assets/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
 
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+  // Ваши настройки, например, для иконок [2]
+  icons: {
+    defaultSet: 'mdi', // или 'fa' для Font Awesome
+  },
+})
+
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
